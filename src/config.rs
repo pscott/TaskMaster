e3c4dde5f6d5a84f51070da05193230fab7675fb
pscott <scottpiriou@gmail.com>
2020-08-27 18:42:43 +0200

    Add stub code for config

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,33 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 #[derive(Debug)]
 /// Configuration structure used to run a task.
 pub struct Config {
-	/// Command to run.
-	cmd: String,
-	/// Number of processors this task should run with.
-	// u16 is fine because we are not expecting a machine to run more
-	// than 2^16 proc at any single time.
-	numprocs: u16,
-	/// Working directory of the task.
-	workingdir: PathBuf,
+    /// Command to run.
+    cmd: String,
+    /// Number of processors this task should run with.
+    // u16 is fine because we are not expecting a machine to run more
+    // than 2^16 proc at any single time.
+    numprocs: u16,
+    /// Working directory of the task.
+    workingdir: PathBuf,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            cmd: "ls".into(),
+            numprocs: 1,
+            workingdir: PathBuf::from("/tmp"),
+        }
+    }
+}
+
+impl std::convert::TryFrom<&Path> for Config {
+    type Error = std::io::Error;
+
+    fn try_from(_file_name: &Path) -> Result<Self, Self::Error> {
+        // TODO: deserialize from yaml file
+        Ok(Self::default())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 mod config;
 
-fn main() {
-    println!("Hello, world!");
+use config::Config;
+use std::convert::TryFrom;
+use std::path::Path;
+
+fn main() -> Result<(), std::io::Error> {
+    let path = Path::new("config.yaml");
+    let config = Config::try_from(path)?;
+    println!("Here's your config: {:#?}", config);
+    Ok(())
 }
