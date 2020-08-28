1678a3b442bef952fde24f94703555b1d8289d43
pscott <scottpiriou@gmail.com>
2020-08-28 19:00:49 +0200

    Add create_command macro and comment code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,118 @@
+extern crate liner;
 mod config;
 
 use config::Config;
+use liner::{Completer, Context};
 use std::convert::TryFrom;
 use std::path::Path;
 
-extern crate liner;
-use liner::{Completer, Context};
+#[derive(Debug)]
+/// Command that will be executed.
+enum Command<'a> {
+    /// Activates any updates in config for process/group.
+    Add(&'a [&'a str]),
+    /// Clear one or multiple process’ log files
+    Clear(&'a [&'a str]),
+    /// Exit TaskMaster.
+    Exit,
+    /// Get the PID of one or multiple child processes.
+    PID(&'a [&'a str]),
+    /// Removes process/group from active config.
+    Remove(&'a [&'a str]),
+    /// Restart multiple processes or groups.
+    /// Note: restart does not reread config files. For that, see reread and update.
+    Restart(&'a [&'a str]),
+    /// Start one or multiple processes/groups.
+    Start(&'a [&'a str]),
+    /// Get status on one or multiple named processes.
+    Status(&'a str),
+    /// Stop one or multiple processes or groups.
+    Stop(&'a [&'a str]),
+    /// Reload config and add/remove as necessary, and will restart affected programs.
+    Update(&'a [&'a str]),
+}
 
+#[derive(Debug)]
+/// Errors that could appear when one tries to parse an input into a Command.
+enum CommandParsingError {
+    EmptyCommand,
+    UnknownCommand(String),
+    UnexpectedArguments,
+    MissingArguments,
+}
+
+impl CommandParsingError {
+    fn display(&self) {
+        match self {
+            Self::UnknownCommand(s) => eprintln!("{}", s),
+            Self::UnexpectedArguments => eprintln!("Unexpected arguments"),
+            Self::MissingArguments => eprintln!("Missing arguments"),
+            _ => {}
+        }
+    }
+}
+
+/// Creates a new `Command` based on the arguments provided.
+/// Example:
+/// ```rust
+/// create_command!(args, Exit, zero_arg);
+/// ```
+/// Will create a `Command::Exit`, and will error if the number of
+/// additional arguments (after the first argument) is not 0.
+/// Possible values are: zero_arg, one_arg, multiple_args.
+macro_rules! create_command {
+    ($args:ident, $name:ident, zero_arg) => {
+        if $args.len() == 1 {
+            Ok(Command::$name)
+        } else {
+            Err(CommandParsingError::UnexpectedArguments)
+        }
+    };
+    ($args:ident, $name:ident, one_arg) => {
+        if $args.len() == 2 {
+            Ok(Command::$name($args[1]))
+        } else if $args.len() < 2 {
+            Err(CommandParsingError::MissingArguments)
+        } else {
+            Err(CommandParsingError::UnexpectedArguments)
+        }
+    };
+    ($args:ident, $name:ident, multiple_args) => {
+        if $args.len() > 1 {
+            Ok(Command::$name(&$args[1..]))
+        } else {
+            Err(CommandParsingError::MissingArguments)
+        }
+    };
+}
+
+impl<'a> std::convert::TryFrom<&'a [&'a str]> for Command<'a> {
+    type Error = CommandParsingError;
+
+    fn try_from(data: &'a [&'a str]) -> Result<Self, Self::Error> {
+        match data.get(0) {
+            None => Err(Self::Error::EmptyCommand),
+            Some(&command) => match command {
+                "add" => create_command!(data, Add, multiple_args),
+                "clear" => create_command!(data, Clear, multiple_args),
+                "exit" => create_command!(data, Exit, zero_arg),
+                "pid" => create_command!(data, PID, multiple_args),
+                "remove" => create_command!(data, Remove, multiple_args),
+                "restart" => create_command!(data, Restart, multiple_args),
+                "start" => create_command!(data, Start, multiple_args),
+                "status" => create_command!(data, Status, one_arg),
+                "stop" => create_command!(data, Stop, multiple_args),
+                "update" => create_command!(data, Update, multiple_args),
+                other => Err(Self::Error::UnknownCommand(format!(
+                    "Unknown command: {}",
+                    other
+                ))),
+            },
+        }
+    }
+}
+
+/// Placeholder struct for Completer.
 struct EmptyCompleter;
 
 impl Completer for EmptyCompleter {
@@ -15,134 +121,24 @@ impl Completer for EmptyCompleter {
     }
 }
 
-#[derive(Debug)]
-enum Command<'a> {
-    Clear(&'a [&'a str]),
-    ClearAll,
-    Exit,
-    Restart(&'a [&'a str]),
-    RestartAll,
-    Start(&'a [&'a str]),
-    StartAll,
-    Status,
-    Stop(&'a [&'a str]),
-    StopAll,
-    Update,
-    UpdateAll,
-}
-
-#[derive(Debug)]
-enum CommandError {
-    EmptyCommand,
-    UnknownCommand(String),
-    UnexpectedArguments,
-    MissingArguments,
-}
-
-impl CommandError {
-    fn display(&self) {
-        match self {
-            Self::UnknownCommand(s) => eprintln!("{}", s),
-            Self::UnexpectedArguments => eprintln!("Unexpected arguments."),
-            Self::MissingArguments => eprintln!("Missing arguments."),
-            _ => {},
-        }
-    }
-}
-
-impl<'a> std::convert::TryFrom<&'a [&'a str]> for Command<'a> {
-    type Error = CommandError;
-
-    // TODO add a macro to avoid code duplication
-    fn try_from(data: &'a [&'a str]) -> Result<Self, Self::Error> {
-        match data.get(0) {
-            Some(&command) => match command {
-                "clear" => {
-                    if data[1..].is_empty() {
-                        Err(Self::Error::MissingArguments)
-                    } else if data[1] == "all" {
-                        Ok(Self::ClearAll)
-                    } else {
-                        Ok(Self::Clear(&data[1..]))
-                    }
-                }
-                "exit" => {
-                    if data[1..].is_empty() {
-                        Ok(Self::Exit)
-                    } else {
-                        Err(Self::Error::UnexpectedArguments)
-                    }
-                }
-                "restart" => {
-                    if data[1..].is_empty() {
-                        Err(Self::Error::MissingArguments)
-                    } else if data[1] == "all" {
-                        Ok(Self::RestartAll)
-                    } else {
-                        Ok(Self::Restart(&data[1..]))
-                    }
-                }
-                "start" => {
-                    if data[1..].is_empty() {
-                        Err(Self::Error::MissingArguments)
-                    } else if data[1] == "all" {
-                        Ok(Self::StartAll)
-                    } else {
-                        Ok(Self::Start(&data[1..]))
-                    }
-                }
-                "status" => {
-                    if data[1..].is_empty() {
-                        Ok(Self::Status)
-                    } else {
-                        Err(Self::Error::UnexpectedArguments)
-                    }
-                }
-                "stop" => {
-                    if data[1..].is_empty() {
-                        Err(Self::Error::MissingArguments)
-                    } else if data[1] == "all" {
-                        Ok(Self::StopAll)
-                    } else {
-                        Ok(Self::Stop(&data[1..]))
-                    }
-                }
-                "update" => {
-                    if data[1..].is_empty() {
-                        Ok(Self::Update)
-                    } else if data[1] == "all" {
-                        Ok(Self::UpdateAll)
-                    } else {
-                        Err(Self::Error::UnexpectedArguments)
-                    }
-                }
-                other => Err(Self::Error::UnknownCommand(format!(
-                    "Unknown command: {}",
-                    other
-                ))),
-            },
-            None => Err(Self::Error::EmptyCommand),
-        }
-    }
-}
-
 fn main() -> Result<(), std::io::Error> {
     let path = Path::new("config.yaml");
     let _config = Config::try_from(path)?;
+
     let mut con = Context::new();
 
     loop {
-        let res = con.read_line("$ ", None, &mut EmptyCompleter)?;
+        let line = con.read_line("$ ", None, &mut EmptyCompleter)?;
 
-        let words = res.split_ascii_whitespace().collect::<Vec<&str>>();
-        let cmd = Command::try_from(&words[..]);
+        let args = line.split_ascii_whitespace().collect::<Vec<&str>>();
+        let cmd = Command::try_from(&args[..]);
         match cmd {
             Ok(Command::Exit) => break,
-            Ok(_) => {},
+            Ok(_) => {} // We will need to actually execute the command here!
             Err(cmd_err) => cmd_err.display(),
         }
 
-        con.history.push(res.into())?;
+        con.history.push(line.into())?;
     }
     Ok(())
 }
