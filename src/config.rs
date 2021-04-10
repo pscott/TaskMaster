213278f66d93959f95311d6321f17e28a9d8f8ce
Antoine Barthelemy <abarthel@student.42.fr>
2021-04-10 13:24:48 +0200

    feat(config): add comment and mandatory/optional to fcgi-prog eventlistener section

--- src/config.rs
+++ src/config.rs
@@ -874,38 +874,271 @@ pub struct FcgiProgram {
 
 }
 
+/// Taskmaster allows specialized homogeneous process groups (“event listener pools”) to be defined
+/// within the configuration file.
+/// These pools contain processes that are meant to receive and respond to event notifications
+/// from taskmaster’s event system.
+/// http://supervisord.org/events.html#event-types
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(deny_unknown_fields)]
 pub struct EventListener {
-    command: String,
-    process_name: String,
-    numprocs: u16,
-    events: String, //PROCESS_STATE
-    buffer_size: i32,
-    directory: PathBuf,
-    umask: String, // https://docs.rs/umask/1.0.0/umask/
-    priority: i32,
-    autostart: bool,
-    autorestart: Restart,
-    startsecs: i32,
-    startretries: i32,
-    exitcodes: Vec<i32>,
-    stopsignal: Vec<String>,
-    stopwaitsecs: i32,
-    stopasgroup: bool,
-    killasgroup: bool,
-    user: String,
-    redirect_stderr: bool,
-    stdout_logfile: PathBuf,
-    stdout_logfile_maxbytes: i32,
-    stdout_logfile_backups: i32,
-    stdout_events_enabled: bool,
-    stderr_logfile: PathBuf,
-    stderr_logfile_maxbytes: i32,
-    stderr_logfile_backups: i32,
-    stderr_events_enabled: bool,
-    environment: HashMap<String, String>,
-    serverurl: String,
+
+    /// The event listener pool’s event queue buffer size. When a listener pool’s event buffer
+    /// is overflowed (as can happen when an event listener pool cannot keep up with all of the
+    /// events sent to it),the oldest event in the buffer is discarded.
+    /// Default: `10`
+    /// Required: No
+    buffer_size: Option<i32>,
+
+    /// Event types that may be subscribed to by event listeners are predefined by taskmaster
+    /// and fall into several major categories, including “process state change”, “process communication”,
+    /// and “supervisor state change” events.
+    /// Default: No default value
+    /// Required: No
+    events: Option<HashMap<String, String>>,
+
+    /// A pkg_resources entry point string that resolves to a Python callable.
+    /// The default value is taskmaster.dispatchers:default_handler.
+    /// Specifying an alternate result handler is a very uncommon thing to need to do,
+    /// and as a result, how to create one is not documented.
+    /// Default: `??`
+    /// Required: No
+    result_handler: Option<String>,
+
+    /// The command that will be run when this program is started.
+    /// The command can be either absolute (e.g. /path/to/programname) or relative (e.g. programname).
+    /// If it is relative, the taskmasterd’s environment $PATH will be searched for the executable.
+    /// Programs can accept arguments, e.g. /path/to/program foo bar.
+    /// The command line can use double quotes to group arguments with spaces in them to pass to the program,
+    /// e.g. /path/to/program/name -p "foo bar".
+    /// Note that the value of command may include Python string expressions,
+    /// e.g. /path/to/programname --port=80%(process_num)02d might expand to /path/to/programname --port=8000 at runtime.
+    /// String expressions are evaluated against a dictionary containing the keys
+    /// group_name, host_node_name, program_name, process_num, numprocs,
+    /// here (the directory of the taskmasterd config file), and all taskmasterd’s environment variables prefixed
+    /// with ENV_. Controlled programs should themselves not be daemons,
+    /// as taskmasterd assumes it is responsible for daemonizing its subprocesses (see Nondaemonizing of Subprocesses).
+    /// Default: No default
+    /// Required: Yes
+    command: String, // (PathBuf, String, ...)
+
+    /// A Python string expression that is used to compose the taskmaster process name for this process.
+    /// You usually don’t need to worry about setting this unless you change numprocs.
+    /// The string expression is evaluated against a dictionary that includes
+    /// group_name, host_node_name, process_num, program_name, and here (the directory of the taskmasterd config file).
+    /// Default: %(program_name)s
+    /// Required: No
+    process_name: Option<String>,
+
+    /// Taskmaster will start as many instances of this program as named by numprocs.
+    /// Note that if numprocs > 1, the process_name expression must include %(process_num)s
+    /// (or any other valid Python string expression that includes process_num) within it.
+    /// Default: 1
+    /// Required: No
+    numprocs: Option<u16>,
+
+    /// An integer offset that is used to compute the number at which numprocs starts.
+    /// Default: 0
+    /// Required: No
+    numprocs_start: Option<u16>,
+
+    /// The relative priority of the program in the start and shutdown ordering.
+    /// Lower priorities indicate programs that start first and shut down last at
+    /// startup and when aggregate commands are used in various clients
+    /// (e.g. “start all”/”stop all”).
+    /// Higher priorities indicate programs that start last and shut down first.
+    /// Default: `999`
+    /// Required: No
+    priority: Option<i32>,
+
+    /// If true, this program will start automatically when taskmasterd is started.
+    /// Default: `true`
+    /// Required: No
+    autostart: Option<bool>,
+
+    /// The total number of seconds which the program needs to stay running after a startup
+    /// to consider the start successful (moving the process from the STARTING state to the RUNNING state).
+    /// Set to 0 to indicate that the program needn’t stay running for any particular amount of time.
+    /// Default: `1`
+    /// Required: No
+    startsecs: Option<i32>,
+
+    /// The number of serial failure attempts that taskmasterd will allow when attempting to start
+    /// the program before giving up and putting the process into an FATAL state.
+    /// Default: `3`
+    /// Required: No
+    startretries: Option<i32>,
+
+    /// Specifies if taskmasterd should automatically restart a process if it exits when it is
+    /// in the RUNNING state.
+    /// May be one of `false`, `unexpected`, or `true`.
+    /// If `false`, the process will not be autorestarted.
+    /// If `unexpected`, the process will be restarted when the program exits with an
+    /// exit code that is not one of the exit codes associated with this process’ configuration.
+    /// If `true`, the process will be unconditionally restarted when it exits, without regard to its exit code.
+    /// Default: `unexpected`
+    /// Required: No.
+    autorestart: Option<Restart>,
+
+    /// The list of “expected” exit codes for this program used with autorestart.
+    /// If the autorestart parameter is set to `unexpected`, and the process exits
+    /// in any other way than as a result of a tadskmaster stop request,
+    /// taskmasterd will restart the process if it exits with an exit code that is not defined in this list.
+    /// Default: `0`
+    /// Required: No
+    exitcodes: Option<Vec<i32>>,
+
+    /// The signal used to kill the program when a stop is requested.
+    /// This can be any of `TERM`, `HUP`, `INT`, `QUIT`, `KILL`, `USR1`, or `USR2`.
+    /// Default: `TERM`
+    /// Required: No
+    stopsignal: Option<Vec<String>>,
+
+    /// The number of seconds to wait for the OS to return a `SIGCHLD` to taskmasterd after the program
+    /// has been sent a stopsignal.
+    /// If this number of seconds elapses before taskmasterd receives a SIGCHLD from the process,
+    /// taskmasterd will attempt to kill it with a final `SIGKILL`.
+    /// Default: `10`
+    /// Required: No
+    stopwaitsecs: Option<i32>,
+
+    /// If `true`, the flag causes taskmaster to send the stop signal to the whole process group
+    /// and implies killasgroup is `true`.
+    /// This is useful for programs, such as Flask in debug mode, that do not propagate
+    /// stop signals to their children, leaving them orphaned.
+    /// Default: `false`
+    /// Required: No
+    stopasgroup: Option<bool>,
+
+    /// If true, when resorting to send `SIGKILL` to the program to terminate it send it to its
+    /// whole process group instead, taking care of its children as well,
+    /// useful e.g with Python programs using multiprocessing.
+    /// Default: `false`
+    /// Required: No
+    killasgroup: Option<bool>,
+
+    /// Instruct taskmasterd to use this UNIX user account as the account which runs the program.
+    /// The user can only be switched if taskmasterd is run as the root user.
+    /// If taskmasterd can’t switch to the specified user, the program will not be started.
+    /// Default: Do not switch users
+    /// Required: No
+    user: Option<String>,
+
+    /// If `true`, cause the process’ stderr output to be sent back to taskmasterd on its
+    /// stdout file descriptor
+    /// (in UNIX shell terms, this is the equivalent of executing /the/program 2>&1).
+    /// Default: `false`
+    /// Required: No
+    redirect_stderr: Option<bool>,
+
+    /// Put process stdout output in this file (and if redirect_stderr is true, also place
+    /// stderr output in this file).
+    /// If stdout_logfile is unset or set to `AUTO`, taskmaster will automatically choose a file location.
+    /// If this is set to `NONE`, taskmasterd will create no log file.
+    /// `AUTO` log files and their backups will be deleted when taskmasterd restarts.
+    /// The stdout_logfile value can contain Python string expressions that will evaluated against
+    /// a dictionary that contains the keys
+    /// `group_name`, `host_node_name`, `process_num`, `program_name`, and `here` (the directory of the taskmasterd config file).
+    /// Default: `AUTO`
+    /// Required: No
+    stdout_logfile: Option<PathBuf>,
+
+    /// The maximum number of bytes that may be consumed by stdout_logfile before it is rotated
+    /// (suffix multipliers like “KB”, “MB”, and “GB” can be used in the value).
+    /// Set this value to 0 to indicate an unlimited log size.
+    /// Default: `50MB`
+    /// Required: No
+    stdout_logfile_maxbytes: Option<i32>,
+
+    /// The number of stdout_logfile backups to keep around resulting from process stdout log file rotation.
+    /// If set to 0, no backups will be kept.
+    /// Default: `10`
+    /// Required: No
+    stdout_logfile_backups: Option<i32>,
+
+    /// Max number of bytes written to capture FIFO when process is in “stdout capture mode” (see Capture Mode).
+    /// Should be an integer (suffix multipliers like “KB”, “MB” and “GB” can used in the value).
+    /// If this value is 0, process capture mode will be off.
+    /// Default: `0`
+    /// Required: No
+    stdout_capture_maxbytes: Option<i32>,
+
+    /// If `true`, PROCESS_LOG_STDOUT events will be emitted when the process writes to its stdout file descriptor.
+    /// The events will only be emitted if the file descriptor is not in capture mode at the time the data is received (see Capture Mode).
+    /// Default: `0`
+    /// Required: No
+    stdout_events_enabled: Option<bool>,
+
+    /// If `true`, stdout will be directed to syslog along with the process name.
+    /// Default: `False`
+    /// Required: No
+    stdout_syslog: Option<bool>,
+
+    /// Put process stderr output in this file unless redirect_stderr is `true`.
+    /// Accepts the same value types as stdout_logfile and may contain the same Python string expressions.
+    /// Default: `AUTO`
+    /// Required: No
+    stderr_logfile: Option<PathBuf>,
+
+    /// The maximum number of bytes before logfile rotation for stderr_logfile.
+    /// Accepts the same value types as stdout_logfile_maxbytes.
+    /// Default: `50MB`
+    /// Required: No
+    stderr_logfile_maxbytes: Option<i32>,
+
+    /// The number of backups to keep around resulting from process stderr log file rotation. If set to `0`, no backups will be kept.
+    /// Default: `10`
+    /// Required: No
+    stderr_logfile_backups: Option<i32>,
+
+    /// Max number of bytes written to capture FIFO when process is in “stderr capture mode” (see Capture Mode).
+    /// Should be an integer (suffix multipliers like “KB”, “MB” and “GB” can used in the value).
+    /// If this value is `0`, process capture mode will be off.
+    /// Default: `0`
+    /// Required: No
+    stderr_capture_maxbytes: Option<i32>,
+
+    /// If `true`, PROCESS_LOG_STDERR events will be emitted when the process writes to its stderr file descriptor.
+    /// The events will only be emitted if the file descriptor is not in capture mode at the time the data is received (see Capture Mode).
+    /// Default: `false`
+    /// Required: No
+    stderr_events_enabled: Option<bool>,
+
+    /// If `true`, stderr will be directed to syslog along with the process name.
+    /// Default: `False`
+    /// Required: No
+    stderr_syslog: Option<bool>,
+
+    /// A list of key/value pairs in the form KEY="val",KEY2="val2" that will be placed in the child process’ environment.
+    /// The environment string may contain Python string expressions that will be evaluated against a dictionary
+    /// containing `group_name`, `host_node_name`, `process_num`, `program_name`, and here (the directory of the taskmasterd config file).
+    /// Values containing non-alphanumeric characters should be quoted (e.g. KEY="val:123",KEY2="val,456").
+    /// Otherwise, quoting the values is optional but recommended.
+    /// Note that the subprocess will inherit the environment variables of the shell used to start “taskmasterd” except for the
+    /// ones overridden here.
+    /// Default: No extra environment
+    /// Required: No
+    environment: Option<HashMap<String, String>>,
+
+    /// A file path representing a directory to which taskmasterd should temporarily chdir before exec’ing the child.
+    /// Default: No chdir (inherit supervisor’s)
+    /// Required: No
+    directory: Option<PathBuf>,
+
+    /// An octal number (e.g. 002, 022) representing the umask of the process.
+    /// Default: No special umask (inherit taskmaster’s)
+    /// Required: No
+    umask: Option<String>, // https://docs.rs/umask/1.0.0/umask/
+
+    /// The URL passed in the environment to the subprocess process as TASKMASTER_SERVER_URL (see taskmaster.childutils)
+    /// to allow the subprocess to easily communicate with the internal HTTP server.
+    /// If provided, it should have the same syntax and structure as the [taskmasterctl] section option of the same name.
+    /// If this is set to `AUTO`, or is unset, taskmaster will automatically construct a server URL, giving preference
+    /// to a server that listens on UNIX domain sockets over one that listens on an internet socket.
+    /// Default: `AUTO`
+    /// Required: No
+    serverurl: Option<String>,
+
 }
 
 #[derive(Serialize, Deserialize, Debug)]
